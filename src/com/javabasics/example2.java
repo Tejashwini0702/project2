528f4538eb8de622eead271788ad5c091ced6dbd
91818 <91818@LAPTOP-OMCDS4TQ>
2022-02-10 00:11:22 +0530

    committed project2

---
+++ src/com/javabasics/example2.java
@@ -0,0 +1,13 @@
+package com.javabasics;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10,b=25,c;
+       c=a+b;
+       
+       System.out.println("sum of these numbers:"+c);
+	}
+
+}
